fd1f3927f1a84868fca8eecb96f82bfcb6095ad3
iamarun8 <arun.kumar@successive.tech>
2020-12-24 18:10:45 +0530

    day 6 task

--- training/src/components/TextField/TextField.jsx
+++ training/src/components/TextField/TextField.jsx
@@ -1,51 +1,31 @@
 import React from 'react';
-import * as style from './style';
 import PropTypes from 'prop-types';
+import { Error, Input } from './style';
 
-function TextField(props) {
-    const { disabled, value, error } = props;
-    if (disabled) {
-        // console.log('disabled is : ',props.disabled);
-        // console.log('--------------------end of disabled-------------------------');
-        return (
-            <>
-                <h2 style={style.h}>This is Disabled Input</h2>
-                <input style={style.disableInput} type="text" defaultValue="Disabled Input" disabled={disabled} />
-            </>
-        );
-    }
-    if (value) {
-        // console.log('value is : ',props.value);
-        // console.log('--------------------end of value-------------------------');
-        return (
-            <>
-                <h2 style={style.h}>A Valid Input</h2>
-                <input style={style.validInput} type="text" defaultValue={value} />
-            </>
-        );
-    }
+const TextField = (props) => {
+    const { value, error, onChange } = props;
     if (error) {
-        // console.log('error is : ',props.error);
-        // console.log('--------------------end of error-------------------------');
         return (
             <>
-                <h2 style={style.h}>An Input with errors</h2>
-                <input style={style.error} type="text" defaultValue={error} />
-                <p style={{ color: 'red' }}> Could not be greater than </p>
+                <Input type="text" value={value} error onChange={onChange} />
+                <Error>{error}</Error>
             </>
         );
     }
-}
-
-TextField.propTypes = {
-    disabled: PropTypes.bool,
-    value: PropTypes.string,
-    error: PropTypes.string,
-};
-TextField.defaultProps = {
-    disabled: '',
-    value: '',
-    error: '',
+    return (
+        <Input type="text"  onChange={onChange} />
+    );
 };
 
 export default TextField;
+TextField.propTypes = {
+    value: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
+    error: PropTypes.string,
+    onChange: PropTypes.func,
+};
+TextField.defaultProps = {
+    disabled: false,
+    error: '',
+    onChange: '',
+};
--- training/src/components/TextField/style.js
+++ training/src/components/TextField/style.js
@@ -1,24 +1,29 @@
-export const Box = {
-    width: '100%',
-    padding: '10px',
-    overflow: 'hidden'
-};
+import styled, { css } from 'styled-components';
 
-export const h = {
-    margin: '5px'
+const Div = styled.div`
+2%;
+`;
+const Error = styled.p`
+color: red;
+`;
+const Input = styled.input`
+width: 100%;
+padding: 0.7%;
+border: 1px solid gray;
+border-radius: 5px;
+color: solid gray;
+${(props) => props.error
+        && css`
+border: 1px solid red;
+color: red;
+`};
 }
+${(props) => (props.value && !props.disabled && !props.error)
+        && css`
+border: 1px solid orange;
+color: black;
+`};
+}
+`;
 
-export const disableInput = {
-    ...Box,
-    color: 'grey',
-};
-
-export const validInput = {
-    ...Box,
-    border: '2px solid grey',
-};
-
-export const error = {
-    ...Box,
-    border: '2px solid red',
-};
+export { Div, Error, Input };
