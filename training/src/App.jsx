c04969c19bbc8ac2f4eef207b442866405a9f32f
iamarun8 <arun.kumar@successive.tech>
2020-12-31 19:26:31 +0530

    day 10 task

--- training/src/App.jsx
+++ training/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Trainee from './pages/Trainee/Trainee';
+import { ButtonAppBar } from './pages/components/Navbar'
+import { Login } from './pages/Login'
 
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Trainee />
+      {/* <ButtonAppBar />
+      <Trainee /> */}
+      <Login />
     </ThemeProvider>
   );
 }
--- training/src/pages/index.js
+++ training/src/pages/index.js
@@ -1,3 +1,4 @@
 export { TextFieldDemo } from './TextFieldDemo';
 export { InputDemo } from './InputDemo';
 export { Trainee } from './Trainee';
+export { ButtonAppBar } from './components/Navbar'
