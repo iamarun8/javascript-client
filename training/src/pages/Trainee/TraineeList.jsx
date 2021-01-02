6ecf2480f357fe18f9acf0028bf9af9d0ba00416
iamarun8 <arun.kumar@successive.tech>
2021-01-02 23:53:46 +0530

    day 13 task

--- training/src/pages/Trainee/TraineeList.jsx
+++ training/src/pages/Trainee/TraineeList.jsx
@@ -5,6 +5,7 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import { AddDialog } from './components/AddDialog';
 import trainees from './data/trainee';
+import { TableComponent } from '../../components/Table';
 
 const useStyles = (theme) => ({
     root: {
@@ -37,7 +38,6 @@ class TraineeList extends React.Component {
         this.setState({
             open: false,
         }, () => {
-            // eslint-disable-next-line no-console
             console.log(data);
         });
     }
@@ -48,10 +48,31 @@ class TraineeList extends React.Component {
         return (
             <>
                 <div className={classes.root}>
-                    <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-                        ADD TRAINEELIST
-          </Button>
+                    <div className={classes.dialog}>
+                        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+                            ADD TRAINEELIST
+                        </Button>
+                    </div>
                     <AddDialog open={open} onClose={this.handleClose} onSubmit={() => this.handleSubmit} />
+                    &nbsp;
+                    &nbsp;
+                    <TableComponent
+                        id="id"
+                        data={trainees}
+                        column={
+                            [
+                                {
+                                    field: 'name',
+                                    label: 'Name',
+                                    align: 'center',
+                                },
+                                {
+                                    field: 'email',
+                                    label: 'Email Address',
+                                },
+                            ]
+                        }
+                    />
                     <ul>
                         {trainees.map(({ name, id }) => (
                             <li key={id}>
